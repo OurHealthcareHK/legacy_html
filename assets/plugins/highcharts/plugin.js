92f439d81f71d22e346dfef67535f5bfb97e7ffe
mistermochi <36605549+mistermochi@users.noreply.github.com>
2019-01-23 03:41:43 +0800

    Update plugin.js

--- assets/plugins/highcharts/plugin.js
+++ assets/plugins/highcharts/plugin.js
@@ -2,9 +2,6 @@ require.config({
     shim: {
         'highcharts': {
             'exports': 'Highcharts',
-            'deps': ['highcharts-base', 'highcharts-exporting']
-        },        
-        'highcharts-exporting': {
             'deps': ['highcharts-base']
         },
         'highcharts-data': ['highcharts']
@@ -12,7 +9,6 @@ require.config({
     paths: {
         'highcharts-base': '//cdnjs.cloudflare.com/ajax/libs/highcharts/7.0.1/highcharts',        
         'highcharts': '//cdnjs.cloudflare.com/ajax/libs/highcharts/7.0.1/highcharts-more',
-        'highcharts-data': '//cdnjs.cloudflare.com/ajax/libs/highcharts/7.0.1/modules/data.src',
-        'highcharts-exporting': '//cdnjs.cloudflare.com/ajax/libs/highcharts/7.0.1/modules/exporting.src'        
+        'highcharts-data': '//cdnjs.cloudflare.com/ajax/libs/highcharts/7.0.1/modules/data.src'    
     }
 });
