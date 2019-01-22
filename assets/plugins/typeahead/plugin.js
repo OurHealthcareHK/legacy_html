215a096fded01a20772c81469cf559b64bc2f9a1
mistermochi <36605549+mistermochi@users.noreply.github.com>
2019-01-22 13:00:37 +0800

    Update plugin.js

--- assets/plugins/typeahead/plugin.js
+++ assets/plugins/typeahead/plugin.js
@@ -1,4 +1,7 @@
 require.config({
+    shim: {
+        'typeahead': ['jquery']
+    },
     paths: {
         'typeahead': '//cdnjs.cloudflare.com/ajax/libs/corejs-typeahead/1.2.1/typeahead.bundle'
     }
