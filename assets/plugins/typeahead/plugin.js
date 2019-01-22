4ce77b1ad9303071aafa24df9ffa7b4b5c9520e6
mistermochi <36605549+mistermochi@users.noreply.github.com>
2019-01-22 13:09:23 +0800

    Update plugin.js

--- assets/plugins/typeahead/plugin.js
+++ assets/plugins/typeahead/plugin.js
@@ -1,8 +1,10 @@
 require.config({
     shim: {
         'typeahead': ['jquery']
+        'bloodhound': ['jquery']
     },
     paths: {
-        'typeahead': '//cdnjs.cloudflare.com/ajax/libs/corejs-typeahead/1.2.1/typeahead.bundle'
+        'typeahead': '//cdnjs.cloudflare.com/ajax/libs/corejs-typeahead/1.2.1/typeahead.jquery',
+        'bloodhound': '//cdnjs.cloudflare.com/ajax/libs/corejs-typeahead/1.2.1/bloodhound'
     }
 });
