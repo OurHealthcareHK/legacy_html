24ec0bdb19bb22a8f0d5b1d636a1dfdda2ee0b7a
mistermochi <36605549+mistermochi@users.noreply.github.com>
2019-01-22 15:32:56 +0800

    Update plugin.js

--- assets/plugins/chart-js/plugin.js
+++ assets/plugins/chart-js/plugin.js
@@ -1,5 +1,9 @@
 require.config({
+    shim: {
+        'chart-js': ['moment']
+    },
     paths: {
         'chart-js': '//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.bundle.min'
+        'moment': '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.23.0/moment.min'
     }
 });
