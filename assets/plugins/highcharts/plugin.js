51d4314c422fcd13f582528e9f8829f71642d1cf
mistermochi <36605549+mistermochi@users.noreply.github.com>
2019-01-23 03:57:19 +0800

    Update plugin.js

--- assets/plugins/highcharts/plugin.js
+++ assets/plugins/highcharts/plugin.js
@@ -2,10 +2,10 @@ require.config({
     shim: {
     },
     paths: {
-        highcharts: [
+        'highcharts': [
             "//code.highcharts.com/highcharts",
             "//code.highcharts.com/highcharts-more"
-        ]
+        ],
         'highcharts-data': '//cdnjs.cloudflare.com/ajax/libs/highcharts/7.0.1/modules/data.src'    
     }
 });
