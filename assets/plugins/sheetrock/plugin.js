afb17ca2d3b014f41a7f79e4efb066bce24a17bb
mistermochi <36605549+mistermochi@users.noreply.github.com>
2019-01-22 13:36:05 +0800

    Create plugin.js

---
+++ assets/plugins/sheetrock/plugin.js
@@ -0,0 +1,8 @@
+require.config({
+    shim: {
+        'sheetrock': ['jquery']
+    },
+    paths: {
+        'sheetrock': '//cdnjs.cloudflare.com/ajax/libs/jquery-sheetrock/1.1.4/dist/sheetrock.min'
+    }
+});
