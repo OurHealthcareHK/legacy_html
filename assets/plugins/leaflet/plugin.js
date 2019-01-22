1f960387a7a4603061080d445519cd5ce85d1397
mistermochi <36605549+mistermochi@users.noreply.github.com>
2019-01-22 23:44:59 +0800

    Update plugin.js

--- assets/plugins/leaflet/plugin.js
+++ assets/plugins/leaflet/plugin.js
@@ -1,5 +1,10 @@
 require.config({
+    shim: {
+        'leaflet-mapbox-gl': ['leaflet', 'mapbox-gl']
+    },
     paths: {
-        'typeahead': '//unpkg.com/leaflet@1.3.4/dist/leaflet'
+        'leaflet': '//unpkg.com/leaflet@1.3.4/dist/leaflet',
+        'leaflet-mapbox-gl': '//cdn.klokantech.com/mapbox-gl-leaflet/latest/leaflet-mapbox-gl',
+        'mapbox-gl': '//cdn.klokantech.com/mapbox-gl-js/v0.43.0/mapbox-gl'
     }
 });
