865f01d71972e221b18834c10eae6b9aa5138897
mistermochi <36605549+mistermochi@users.noreply.github.com>
2019-01-23 00:58:32 +0800

    Create plugin.js

---
+++ assets/plugins/highcharts/plugin.js
@@ -0,0 +1,12 @@
+require.config({
+    shim: {
+        'typeahead': ['jquery'],
+        'bloodhound': ['jquery']
+    },
+    paths: {
+        'highstock': '//code.highcharts.com/stock/highstock',
+        'highcharts-data': '//code.highcharts.com/modules/data',
+        'highcharts-exporting': '//code.highcharts.com/modules/exporting',
+        
+    }
+});
