2d0bd6f2b39c5fca56bd18b5e31b55654e7ac843
mistermochi <36605549+mistermochi@users.noreply.github.com>
2019-01-22 13:29:48 +0800

    Update plugin.js

--- assets/plugins/chart-js/plugin.js
+++ assets/plugins/chart-js/plugin.js
@@ -1,9 +1,11 @@
 require.config({
     shim: {
-        'chart-js': ['moment']
+        'moment-tc': ['moment']
+        'chart-js': ['moment-tc']
     },
     paths: {
         'chart-js': '//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min',
-        'moment': 'assets/plugins/fullcalendar/js/moment.min'
+        'moment': 'assets/plugins/fullcalendar/js/moment.min',
+        'moment-tc': '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/locale/zh-hk.js'
     }
 });
