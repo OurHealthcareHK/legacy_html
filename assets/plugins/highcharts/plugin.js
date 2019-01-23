e106606eab829d01e857e9b09d8ced27e0ab2fc4
mistermochi <36605549+mistermochi@users.noreply.github.com>
2019-01-23 10:43:08 +0800

    Update plugin.js

--- assets/plugins/highcharts/plugin.js
+++ assets/plugins/highcharts/plugin.js
@@ -1,11 +1,46 @@
-require.config({    
-    shim: {
-    },
+require.config({   
     paths: {
-        'highcharts': [
-            "//code.highcharts.com/highcharts",
-            "//code.highcharts.com/highcharts-more"
-        ],
-        'highcharts-data': '//cdnjs.cloudflare.com/ajax/libs/highcharts/7.0.1/modules/data.src'    
+        'jquery': 'https://code.jquery.com/jquery-3.3.1.slim.min',
+
+        'highcharts-base': 'https://cglearn.codelight.eu/js/lib/highcharts/highcharts-6.1.1/highcharts',
+        'highcharts': 'https://cglearn.codelight.eu/js/lib/highcharts/highcharts-6.1.1/highcharts-more.src',
+        'highcharts-exporting': 'https://cglearn.codelight.eu/js/lib/highcharts/highcharts-6.1.1/modules/exporting',
+        'highcharts-wordcloud': 'https://cglearn.codelight.eu/js/lib/highcharts/highcharts-6.1.1/modules/wordcloud',
+        'highcharts-sunburst': 'https://cglearn.codelight.eu/js/lib/highcharts/highcharts-6.1.1/modules/sunburst',
+        'highcharts-annotations': 'https://cglearn.codelight.eu/js/lib/highcharts/highcharts-6.1.1/modules/annotations',
+        'highcharts-streamgraph':  'https://cglearn.codelight.eu/js/lib/highcharts/highcharts-6.1.1/modules/streamgraph',
+        'highcharts-serieslabel':  'https://cglearn.codelight.eu/js/lib/highcharts/highcharts-6.1.1/modules/series-label',
+        'highcharts-data':  'https://cglearn.codelight.eu/js/lib/highcharts/highcharts-6.1.1/modules/data',
+
+    },
+    "shim": {
+        'highcharts': {
+            'exports': 'Highcharts',
+            'deps': ['jquery', 'highcharts-base', 'highcharts-exporting', 'highcharts-wordcloud', 'highcharts-sunburst', 'highcharts-annotations', 'highcharts-data', 'highcharts-streamgraph', 'highcharts-serieslabel']
+        },
+        'highcharts-base': {
+            deps: ['jquery']
+        },
+        'highcharts-exporting': {
+            'deps': ['highcharts-base']
+        },
+        'highcharts-wordcloud': {
+            'deps': ['highcharts-base']
+        },
+        'highcharts-sunburst': {
+            'deps': ['highcharts-base']
+        },
+        'highcharts-annotations': {
+            'deps': ['highcharts-base']
+        },
+        'highcharts-streamgraph': {
+            'deps': ['highcharts-base']
+        },
+        'highcharts-serieslabel': {
+            'deps': ['highcharts-base']
+        },
+        'highcharts-data': {
+            'deps': ['highcharts-base']
+        }
     }
 });
