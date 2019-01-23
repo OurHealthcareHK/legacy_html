46dc6e8cb40d44edd7216ab5c91b2f10b1d8cede
mistermochi <36605549+mistermochi@users.noreply.github.com>
2019-01-23 10:54:16 +0800

    Update plugin.js

--- assets/plugins/highcharts/plugin.js
+++ assets/plugins/highcharts/plugin.js
@@ -5,10 +5,6 @@ require.config({
         'highcharts-base': 'https://cglearn.codelight.eu/js/lib/highcharts/highcharts-6.1.1/highcharts',
         'highcharts': 'https://cglearn.codelight.eu/js/lib/highcharts/highcharts-6.1.1/highcharts-more.src',
         'highcharts-exporting': 'https://cglearn.codelight.eu/js/lib/highcharts/highcharts-6.1.1/modules/exporting',
-        'highcharts-wordcloud': 'https://cglearn.codelight.eu/js/lib/highcharts/highcharts-6.1.1/modules/wordcloud',
-        'highcharts-sunburst': 'https://cglearn.codelight.eu/js/lib/highcharts/highcharts-6.1.1/modules/sunburst',
-        'highcharts-annotations': 'https://cglearn.codelight.eu/js/lib/highcharts/highcharts-6.1.1/modules/annotations',
-        'highcharts-streamgraph':  'https://cglearn.codelight.eu/js/lib/highcharts/highcharts-6.1.1/modules/streamgraph',
         'highcharts-serieslabel':  'https://cglearn.codelight.eu/js/lib/highcharts/highcharts-6.1.1/modules/series-label',
         'highcharts-data':  'https://cglearn.codelight.eu/js/lib/highcharts/highcharts-6.1.1/modules/data',
 
@@ -16,24 +12,12 @@ require.config({
     "shim": {
         'highcharts': {
             'exports': 'Highcharts',
-            'deps': ['jquery', 'highcharts-base', 'highcharts-exporting', 'highcharts-wordcloud', 'highcharts-sunburst', 'highcharts-annotations', 'highcharts-data', 'highcharts-streamgraph', 'highcharts-serieslabel']
+            'deps': ['jquery', 'highcharts-base', 'highcharts-exporting', 'highcharts-data', 'highcharts-serieslabel']
         },
         'highcharts-base': {
             deps: ['jquery']
         },
         'highcharts-exporting': {
-            'deps': ['highcharts-base']
-        },
-        'highcharts-wordcloud': {
-            'deps': ['highcharts-base']
-        },
-        'highcharts-sunburst': {
-            'deps': ['highcharts-base']
-        },
-        'highcharts-annotations': {
-            'deps': ['highcharts-base']
-        },
-        'highcharts-streamgraph': {
             'deps': ['highcharts-base']
         },
         'highcharts-serieslabel': {
