a7ff19d4a0222ba3dae16f08a79f3e2e4bb255a8
mistermochi <36605549+mistermochi@users.noreply.github.com>
2019-01-23 19:17:50 +0800

    Update plugin.js

--- assets/plugins/highstock/plugin.js
+++ assets/plugins/highstock/plugin.js
@@ -1,6 +1,6 @@
 require.config({
     paths: {
-        'highstock': '//cdnjs.cloudflare.com/ajax/libs/highstock/6.0.3/highstock',
+        'highstock': '//cdnjs.cloudflare.com/ajax/libs/highstock/6.0.3/highstock.src',
         'moment': '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.13.0/moment.min',
         'moment_tw': '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.13.0/locale/zh-tw'
     }
